5f3c494c37f16fcfdbc0d6f7fbecef125cc0b92e
Lucas de Oliveira <lucasoliveira13670@gmail.com>
2021-11-01 15:12:36 -0400

    commit

--- src/components/creditCard.jsx
+++ src/components/creditCard.jsx
@@ -42,39 +42,41 @@ export function CreditCard() {
             <Image src={BarcodeIcon} alt="barcode" width={20} height={20} />
           </button>
         </div>
-        <section className={styles.formContent}>
-          <fieldset>
-            <label htmlFor="">NÚMERO DO CARTÃO *</label>
-            <input type="number" name="" placeholder="0000 0000 0000 0000" />
-          </fieldset>
-          <fieldset>
-            <label htmlFor="">NOME IMPRESSO NO CARTÃO *</label>
-            <input type="text" name="" />
-          </fieldset>
-          <div className={styles.dateCVV}>
-            <div>
-              <label htmlFor="">MÊS E ANO DO VENCIMENTO *</label>
-              <input type="text" name="" placeholder="MM/AA" />
+        {isCreditCardSelected && (
+          <section className={styles.formContent}>
+            <fieldset>
+              <label htmlFor="">NÚMERO DO CARTÃO *</label>
+              <input type="number" name="" placeholder="0000 0000 0000 0000" />
+            </fieldset>
+            <fieldset>
+              <label htmlFor="">NOME IMPRESSO NO CARTÃO *</label>
+              <input type="text" name="" />
+            </fieldset>
+            <div className={styles.dateCVV}>
+              <div>
+                <label htmlFor="">MÊS E ANO DO VENCIMENTO *</label>
+                <input type="text" name="" placeholder="MM/AA" />
+              </div>
+              <div>
+                <label htmlFor="">CÓDIGO DE SEGURANÇA (CVV) *</label>
+                <input type="number" name="" placeholder="000" />
+                <Image src={CVVIcon} alt="cvv" width={25} height={25} />
+              </div>
             </div>
-            <div>
-              <label htmlFor="">CÓDIGO DE SEGURANÇA (CVV) *</label>
-              <input type="number" name="" placeholder="000" />
-              <Image src={CVVIcon} alt="cvv" width={25} height={25} />
+            <div className={styles.priceCard}>
+              <h1>R$ 99,90</h1>
+              <span>BRL</span>
             </div>
-          </div>
-          <div className={styles.priceCard}>
-            <h1>R$ 99,90</h1>
-            <span>BRL</span>
-          </div>
-          <div className={styles.termsBox}>
-            <button></button>
-            <span>
-              <strong>Importante:</strong> Estou de acordo com os{' '}
-              <a>termos de adesão</a>
-            </span>
-          </div>
-          <button>Realizar Pagamento</button>
-        </section>
+            <div className={styles.termsBox}>
+              <button></button>
+              <span>
+                <strong>Importante:</strong> Estou de acordo com os{' '}
+                <a>termos de adesão</a>
+              </span>
+            </div>
+            <button>Realizar Pagamento</button>
+          </section>
+        )}
       </form>
     </div>
   );
