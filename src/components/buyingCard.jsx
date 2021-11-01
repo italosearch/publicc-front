eb1ff3ec2a12707b630603c772a7f63b24d40439
Lucas de Oliveira <lucasoliveira13670@gmail.com>
2021-10-31 22:37:26 -0400

    co

--- src/components/buyingCard.jsx
+++ src/components/buyingCard.jsx
@@ -2,5 +2,22 @@ import React from 'react';
 import styles from '../styles/components/buyingCard.module.css';
 
 export function BuyingCard() {
-  return <div className={styles.buyingCardContainer}></div>;
+  return (
+    <div className={styles.buyingCardContainer}>
+      <div className={styles.buyingCard}>
+        <h1></h1>
+        <div>
+          <h2></h2>
+          <span></span>
+        </div>
+        <div>
+          <h1></h1>
+          <div>
+            <h2></h2>
+            <span></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
